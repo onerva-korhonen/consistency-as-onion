fixes in the calculation of spatial consistency (moved io out from the function etc.) + visualization

consistency_as_onion.py
@@ -10,38 +10,75 @@ idea is to grow ROIs as spherical onions starting from set centroids and to see,
 decreases with the increasing radius.
 """
 import numpy as np
+import matplotlib.pylab as plt
+from scipy import io
+import cPickle as pickle
 
 import functions
 import onion_parameters as params
 
-subjectFolders = params.testSubjectFolders
-nSubjects = len(subjectFolders)
-roiInfoFile = '/home/onerva/test-data/group_roi_mask-30-4mm_with_subcortl_and_cerebellum.mat'
-resolution = 4
-distanceMatrixPath = params.distanceMatrixPath
+visualizeOnly = False
+consistencySavePath = params.consistencyVsRadiusPath
+figureSavePath = '/media/onerva/KINGSTON/test-data/outcome/consistency-vs-radius.pdf'
 
-radiusRange = np.arange(1,20,2)
-nRadia = len(radiusRange)
-
-centroids, _, voxelCoords = functions.readROICentroids(roiInfoFile,readVoxels=True)
-nROIs = len(centroids)
-
-consistencies = np.zeros((nSubjects,nRadia,nROIs))
-
-for j, radius in enumerate(radiusRange):
-    if j == 0: # calculating the distance matrix only once
-        roiInfo = functions.defineSphericalROIs(centroids,voxelCoords,radius,resolution=resolution,save=True,savePath=distanceMatrixPath)
-    else:
-        roiInfo = functions.defineSphericalROIs(centroids,voxelCoords,radius,resolution=resolution,distanceMatrixPath=distanceMatrixPath)
-    voxelIndices = roiInfo['ROIVoxels']
-    for i, subject in enumerate(subjectFolders):
+if not visualizeOnly:
+    subjectFolders = params.testSubjectFolders
+    nSubjects = len(subjectFolders)
+    roiInfoFile = '/media/onerva/KINGSTON/test-data/group_roi_mask-30-4mm_with_subcortl_and_cerebellum.mat'
+    resolution = 4
+    distanceMatrixPath = params.distanceMatrixPath
+    
+    radiusRange = np.arange(1,20,2)
+    nRadia = len(radiusRange)
+    
+    centroids, _, voxelCoords = functions.readROICentroids(roiInfoFile,readVoxels=True)
+    nROIs = len(centroids)
+    
+    consistencies = np.zeros((nSubjects,nRadia,nROIs))
+    
+    allVoxelTs = []
+    
+    
+    for i, subject in enumerate(subjectFolders): # reading the data here and not inside the loop to save time
         voxelTsFilePath = subject + params.ROIVoxelTsFileName
-        for k, ROI in enumerate(voxelIndices):
-            consistency = functions.calculateSpatialConsistency(ROI,voxelTsFilePath)
-            consistencies[i,j,k] = consistency
-
+        allVoxelTs.append(io.loadmat(voxelTsFilePath)['roi_voxel_data'][0]['roi_voxel_ts'][0])
+    
+    for j, radius in enumerate(radiusRange):
+        if j == 0: # calculating the distance matrix only once
+            roiInfo = functions.defineSphericalROIs(centroids,voxelCoords,radius,resolution=resolution,save=True,savePath=distanceMatrixPath)
+        else:
+            roiInfo = functions.defineSphericalROIs(centroids,voxelCoords,radius,resolution=resolution,distanceMatrixPath=distanceMatrixPath)
+        voxelIndices = roiInfo['ROIVoxels']
+        for i, subject in enumerate(allVoxelTs):
+            for k, ROI in enumerate(voxelIndices):
+                consistency = functions.calculateSpatialConsistency(ROI,subject)
+                consistencies[i,j,k] = consistency
+                
+    consistencyData = {'radia':radiusRange,'subjects':subjectFolders,'consistencies':consistencies}   
+    with open(consistencySavePath, 'wb') as f:
+        pickle.dump(consistencyData, f, -1)
+      
+else:
+    f = open(consistencySavePath, "rb")
+    consistencyData = pickle.load(f)
+    f.close()
+    consistencies = consistencyData['consistencies']
+    radiusRange = consistencyData['radia']
+    
 meanConsistencies = np.mean(consistencies,axis=(0,2)) # average & std over subjects and ROIs
 stdConsistencies = np.std(consistencies,axis=(0,2))
+    
+fig = plt.figure()
+ax = fig.add_subplot(111)
+#plt.plot(radiusRange,meanConsistencies,ls='',marker='.',color='k')
+plt.errorbar(radiusRange,meanConsistencies,yerr=stdConsistencies,ls='',marker='.',color='k',ecolor='k')
+plt.plot([radiusRange[0],radiusRange[-1]],[0,0],ls='--',color='k',alpha=0.7)
+ax.set_xlabel('Radius (in voxels)')
+ax.set_ylabel('Mean spatial consistency')
+plt.tight_layout()
+plt.savefig(figureSavePath,format='pdf',bbox_inches='tight')
+
+
             
             
             

functions.py
@@ -80,7 +80,7 @@ def getDistanceMatrix(ROICentroids, voxelCoords, save=False, savePath=''):
             pickle.dump(distanceData, f, -1)
     return distanceMatrix
     
-def calculateSpatialConsistency(voxelIndices, voxelTsFilePath, type='pearson c', fTransform=False):
+def calculateSpatialConsistency(voxelIndices, allVoxelTs, type='pearson c', fTransform=False):
     """
     Calculates the spatial consistency of a chunk of voxels. By default,
     spatial consistency is defined as the mean Pearson correlation coefficient
@@ -91,10 +91,7 @@ def calculateSpatialConsistency(voxelIndices, voxelTsFilePath, type='pearson c',
     voxelIdices: np.array, indices of voxels; these indices should refer to voxels' 
             locations in the file containing voxel time series; note that the chunk
             must contain at least one voxel
-    voxelTsFilePath: str, path to a file that contains the voxel time series;
-            the file should contain a dictionary with a key 'roi_voxel_data' (and
-            possible additional keys), value assigned to this key is a structured
-            np.array with a field name 'roi_voxel_ts' (and possible additional 
+    allVoxelTs: structured np.array with a field name 'roi_voxel_ts' (and possible additional 
             fields), this field contains voxel time series
     type: str, definition of spatial consistency to be used; default:
           'pearson c' (mean Pearson correlation coefficient), other options:
@@ -136,8 +133,7 @@ def calculateSpatialConsistency(voxelIndices, voxelTsFilePath, type='pearson c',
         print "Detected an empty ROI, set consistency to 0."
     elif np.amax(voxelIndices.shape) == 1:
         spatialConsistency = 1. # a single voxel is always fully consistent
-    else:
-        allVoxelTs = io.loadmat(voxelTsFilePath)['roi_voxel_data'][0]['roi_voxel_ts'][0]
+    else: 
         voxelTs = allVoxelTs[voxelIndices,:]
         if type == 'pearson c':
             correlations = np.corrcoef(voxelTs)